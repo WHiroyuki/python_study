cce0494ab61ad59cd23f07f84002776d072130ab
WHiroyuki <murakami.hiroyuki@withone.co.jp>
2018-05-13 20:19:05 +0900

    add

---
+++ 基礎編/01.文字列/sample.py
@@ -0,0 +1,88 @@
+#文字列の基礎1
+print('python-izm')
+print("python-izm")
+
+#文字列の基礎2
+test_str = """
+test_1
+test_2
+"""
+print(test_str)
+
+#文字列の連結1
+test_str = 'python'
+test_str = test_str + '-'
+test_str = test_str + 'izm'
+print(test_str)
+
+#文字列の連結2
+test_str = '012'
+test_str += '345'
+test_str += '678'
+test_str += '9'
+print(test_str)
+
+#文字列の連結3
+test_str = '012' * 3
+print(test_str)
+
+#文字列へ変換
+test_integer = 100
+print(str(test_integer) + '円')
+
+#文字列の置換
+test_str = 'python-izm'
+print(test_str.replace('izm', 'ism'))
+
+#文字列の分割
+test_str = 'python-izm'
+print(test_str.split('-'))
+
+#文字列の桁揃え1
+test_str = '1234'
+print(test_str.rjust(10, '0'))
+print(test_str.rjust(10, '!'))
+
+#文字列の桁揃え2
+test_str = '1234'
+print(test_str.zfill(10))
+print(test_str.zfill(3))
+
+#文字列の検索1
+test_str = 'python-izm'
+print(test_str.startswith('python'))
+print(test_str.startswith('izm'))
+
+#文字列の検索2
+test_str = 'python-izm'
+print('z' in test_str)
+print('s' in test_str)
+
+#大文字・小文字変換
+test_str = 'Python-Izm.Com'
+print(test_str.upper())
+print(test_str.lower())
+
+#先頭・末尾の削除
+print('----------------------------------')
+
+test_str = '     python-izm.com'
+print(test_str)
+
+test_str = test_str.lstrip()
+print(test_str)
+
+test_str = test_str.lstrip('python')
+print(test_str)
+
+
+print('----------------------------------')
+
+test_str = 'python-izm.com     '
+print(test_str + '/')
+
+test_str = test_str.rstrip()
+print(test_str + '/')
+
+test_str = test_str.rstrip("com")
+print(test_str)
